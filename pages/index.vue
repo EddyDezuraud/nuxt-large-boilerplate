18cadc225e38373c4f486ca9ce0e86d99b3eb42b
edezuraud <edezuraud@gedicom.fr>
2024-01-11 13:58:05 +0100

    Homepage links

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        Homepage
+        <h1>
+            Homepage
+        </h1>
+        <ul>
+            <li>
+                <NuxtLink to="/sample">Sample 1 module</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/sample2">Sample 2 module</NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>   
